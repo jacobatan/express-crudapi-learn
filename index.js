a7a8441ca4e2eea485cca85b529ece35243f836e
Jacob Tan-WSL <jacob.tan229@gmail.com>
2022-06-25 12:38:37 +1200

    feat: added routes to users.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import usersRoutes from "./routes/users.js";
+
 const app = express();
-const PORT = 5000; // can be any number... 
+const PORT = 5000; // can be any number...
 
 // initialise bodyParser middlware
 app.use(bodyParser.json()); // will be using .json data in our application
 
+/**
+ * starting path, route them to usersRoutes
+ */
+app.use('/users', usersRoutes) 
+
+// ROUTES
+app.get("/", (req, res) => res.send("Hello from home"));
+
 // listen for incoming requests.
-app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))
+app.listen(PORT, () =>
+    console.log(`Server running on port: http://localhost:${PORT}`)
+);
