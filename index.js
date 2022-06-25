99d38faf7d484be587074ac763df2549dde70433
Jacob Tan-WSL <jacob.tan229@gmail.com>
2022-06-25 12:19:40 +1200

    feat: server running

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const app = express();
 const PORT = 5000; // can be any number... 
 
 // initialise bodyParser middlware
-
 app.use(bodyParser.json()); // will be using .json data in our application
 
-
 // listen for incoming requests.
-
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))
