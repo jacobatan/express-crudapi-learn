f6e0a655253a82c834a7246865bb282424315760
Jacob Tan-WSL <jacob.tan229@gmail.com>
2022-06-25 12:52:42 +1200

    feat: PUSH routes

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,31 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 22,
+  },
+];
+
 // all routes here start with /users
-router.get('/', (req, res) => {
-    res.send("Hello");
+router.get("/", (req, res) => {
+  res.send(users);
+});
+
+router.post("/", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send(
+    `User with name ${user.firstName} ${user.lastName} added to the database!`
+  );
 });
 
 export default router;
